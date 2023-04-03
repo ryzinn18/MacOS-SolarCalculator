a3c508b2059adeae2368796b60b06582eb180c0a
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2023-04-03 12:19:28 -0700

    Initial Commit

---
+++ SolarCalculator-Swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SolarCalculator-Swift
+//
+//  Created by Ryan Wright-Zinniger on 4/3/23.
+//  Copyright © 2023 Ryan Zinniger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SolarCalculator-Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SolarCalculator-Swift
+//
+//  Created by Ryan Wright-Zinniger on 4/3/23.
+//  Copyright © 2023 Ryan Zinniger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
